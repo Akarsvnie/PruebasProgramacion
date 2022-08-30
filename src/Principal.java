b1072bebeaac86875b8344d8e32122315553a369
Akarsvnie <akarsvnie@gmail.com>
2022-08-30 18:10:42 -0300

    creacion del proyecto y print de variables

---
+++ src/Principal.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Ariel
+ */
+public class Principal {
+    public static void main(String[] args) {
+        int unEntero = 10;
+        float unFlotante = 2.5f;
+        double unDoble = 12.34;
+        String unaCadena = "esto es una cadena";
+                
+        System.out.println(unEntero);
+        System.out.println(unFlotante);
+        System.out.println(unDoble);
+        System.out.println(unaCadena);
+    }
+}
